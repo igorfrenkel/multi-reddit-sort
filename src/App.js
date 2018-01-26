574b528817bf1ed798b64401fb2ba0d00cab1e1c
Vladimir Vinogradov <vinogvla@gmail.com>
2018-01-25 19:57:35 -0500

    Remove todos (using github issues)

--- src/App.js
+++ src/App.js
@@ -17,29 +17,6 @@ const App = () => (
       <Route path="/auth/reddit/confirm" component={AuthRedditConfirm} />
       <Route path="/multi/:multi" component={MultiRedditViewHoc} />
       <Route path="/" exact component={MultiRedditListHoc} />
-      <hr/>
-      <h3>todo</h3>
-      <ul>
-        <li><strike>render a list of reddits for each multi</strike></li>
-        <li><strike>add another multi with its own state</strike></li>
-        <li><strike>add state to redux</strike></li>
-        <li><strike>unchecking/checking changes state</strike></li>
-        <li>reddit functionality
-          <ul>
-            <li><strike>bake into the app reddit api key</strike></li>
-            <li><strike>exchange code for bearer token</strike></li>
-            <li><strike>make authed call to fetch multis</strike></li>
-            <li><strike>wire multis returned by api to state</strike></li>
-            <li><strike>make authed call to fetch subreddits</strike></li>
-            <li><strike>add refresh link, logout</strike></li>
-            <li>paginate multi and sub calls</li>
-            <li>pull user's list of multis</li>
-            <li>send membership change to reddit</li>
-          </ul>
-        </li>
-        <li>sorted reddit list in MultiRedditView</li>
-        <li>style it</li>
-      </ul>
     </div>
   </BrowserRouter>
 )
